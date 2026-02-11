2d8d257b80551e33eb62f6ea1b25344e6943752f
Karolynevictoria44g <karolyne.santos5@etec.sp.gov.br>
2026-02-11 15:04:09 -0300

    11/02

---
+++ aula01/script.js
@@ -0,0 +1,20 @@
+var nome = "Karolyne"
+
+var idade = 15;
+
+var altura = 1.55;
+
+console.log( nome )
+console.log( idade )
+console.log( altura )
+
+
+
+document.write("<h1>olá, seja bem vindo ao JavaScript</h1>")
+
+document.write( nome + "<br>" );
+document.write( idade + "<br>" );
+document.write( altura + "<br>") ;
+
+console.log( idade * 365 )
+document.write("<p style='color : red'>você já viveu " + idade * 365);
