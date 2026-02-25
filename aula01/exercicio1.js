31a69b912f4782881919f9ceb64683753442316a
Karolynevictoria44g <karolynegreghi9@gmail.com>
2026-02-25 14:57:43 -0300

    25/02

---
+++ aula01/exercicio1.js
@@ -0,0 +1,8 @@
+document.write("<h1> Exercicio 01</h1>")
+var numero = prompt ("O quadrado do numero");
+
+resultado =Number(numero) * Number(numero);
+document.write("O quadrado do numero é " + resultado + "<br>");
+
+resultado = Number(numero) * Number(numero) * Number(numero);
+document.write("o cubo do numero é" + resultado + "<br>");
