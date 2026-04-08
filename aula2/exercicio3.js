44429f71054a27156df108589a6e982a95f1897f
Karolynevictoria44g <karolyne.santos5@etec.sp.gov.br>
2026-04-08 15:01:12 -0300

    08/04

---
+++ aula2/exercicio3.js
@@ -0,0 +1,13 @@
+function calcular()
+{
+let valor, taxa, tempo, resultado;
+
+valor = Number (document.getElementById("valor").value);
+taxa  = Number (document.getElementById("taxa").value);
+tempo = Number (document.getElementById("tempo").value);
+
+resultado = valor + (valor * (taxa * taxa / 100)* tempo);
+
+document.getElementById("resultado").innerHTML =
+"<p> O valor da parcela é " + resultado + "</p>";
+}
