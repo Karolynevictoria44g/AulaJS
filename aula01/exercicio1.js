66f7f2fdd583f1458639be6d88acbe62aef3f0ec
Karolynevictoria44g <karolyne.santos5@etec.sp.gov.br>
2026-04-15 08:05:27 -0300

    Update exercicio1.js

--- aula01/exercicio1.js
+++ aula01/exercicio1.js
@@ -6,3 +6,7 @@ document.write("O quadrado do numero é " + resultado + "<br>");
 
 resultado = Number(numero) * Number(numero) * Number(numero);
 document.write("o cubo do numero é" + resultado + "<br>");
+
+function() { var valor = Number(document.getElementById("valor").value); var quantidade = Number(document.getElementById("quantidade").value); var min = Number(document.getElementById("minutos").value); var calcular; calcular = (valor * quantidade) + (minutos * 0.50); document.getElementById("Resultado").innerHTML = " O resultado é " + calcular + " 
+
+"; }
